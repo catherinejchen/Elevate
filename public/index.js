617907854b9b4e77b9a1798bb38afc2a0897b63b
catherinejchen <59471702+catherinejchen@users.noreply.github.com>
2020-01-12 05:59:26 -0500

    lets do this again

--- public/index.js
+++ public/index.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    var video = document.querySelector("#videoElement");
+/*if(navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}*/
 
 $(".custom-file-input").on("change", function() {
   const fileName = $(this).val().split("\\").pop();
@@ -18,22 +28,8 @@ $('#tester').click(function(e){
           $('#transcript').append(data.transcription);
           for(var i = 0; i < data.analysis.length; i++){
            $('#nlp-text').append("Sentence " + i+1 + ": " + data.analysis[i]["text"]);
-           if(data.analysis[i]["score"] >= 0.8 && data.analysis[i]["magnitude"] >= 0.3)
-           {
-            $('#nlp-score').append("Comment: This is a great sentence and you seem very confident. You should keep this in!");
-           } else if(data.analysis[i]["score"] <= -0.6 && data.analysis[i]["magnitude"] >= 0.3)
-           {
-            $('#nlp-score').append("Comment: You seem a bit distracted here and a tad negative, and generally not as confident in this statement. Try refining it to better reflect you");
-           } else if(data.analysis[i]["score"] >= 0.1 && data.analysis[i]["magnitude"] >= 0.0)
-           {
-            $('#nlp-score').append("Comment: This seems to be an okay part of your speech. I can't really tell. Maybe refine it a bit more to have some heart if needed");
-           } else if(data.analysis[i]["score"] >= 0.0 && data.analysis[i]["magnitude"] >= 0.4)
-           {
-            $('#nlp-score').append("Comment: You seem to have mixed emotions about what you're saying here. Is that what you're trying to convey?");
-           }
-
-          //$('#nlp-score').append("Score: " + data.analysis[i]["score"]);
-          //$('#nlp-mag').append("Magnitude: " + data.analysis[i]["magnitude"]);
+          $('#nlp-score').append("Score: " + data.analysis[i]["score"]);
+          $('#nlp-mag').append("Magnitude: " + data.analysis[i]["magnitude"]);
         }
       },
       fail: function(error) {
