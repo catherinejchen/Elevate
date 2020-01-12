39e8c660a76a86c2a66d989277f3786d98bcb21f
Catherine <catherinejchen@hotmail.com>
2020-01-12 05:41:44 -0500

    re-uploaded files

--- public/script.js
+++ public/script.js
@@ -1,173 +1,60 @@
-*{
-    margin: 0;
-    padding: 0;
-    border: 0;
+var video = document.querySelector('video');
+
+function captureCamera(callback) {
+   navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true
+   }).then(function (camera) {
+      callback(camera);
+   }).catch(function (error) {
+      alert('Unable to capture your camera. Please check console logs.');
+      console.error(error);
+   });
 }
 
-#background {
-    background-image: url(clouds.png), url(hot_air_balloon.png);
-    background-color: rgb(188, 232, 253);
-    background-position: left top, right bottom, left top;
-    background-repeat: repeat, no-repeat, repeat;
-    background-size: 100%, 40%, 100%;
-    background-attachment: fixed;
+function stopRecordingCallback() {
+   video.src = video.srcObject = null;
+   video.muted = false;
+   video.volume = 1;
+   video.src = URL.createObjectURL(recorder.getBlob());
+   recorder.camera.stop();
+   invokeSaveAsDialog(recorder.getBlob());
+   recorder.destroy();
+   recorder = null;
 }
 
-p{
-    font-family: myriad-pro, sans-serif;
-    font-style: normal;
-    letter-spacing: 0.8vw;
-}
+var recorder; // globally accessible
 
-.background-alt{
-    background-image: url(hot_air_balloon.png), url(hot_air_balloon.png);
-    background-color: rgb(247, 247, 247);
-    background-repeat: no-repeat, no-repeat, repeat;
-    background-size: 15%, 10%, 100%;
-    background-position-x: 1vw, 80vw, 0;
-    background-position-y: 10vw, 55vw, 0;
-}
+document.getElementById('btn-start-recording').onclick = function () {
+   this.disabled = true;
+   this.style.background = "#f0f0f0";
+   this.style.color = "#666";
+   document.querySelector('#btn-stop-recording').style.background = "#e74c3c";
+   document.querySelector('#btn-stop-recording').style.color = "#fff";
+   captureCamera(function (camera) {
+      video.muted = true;
+      video.volume = 0;
+      video.srcObject = camera;
 
-.title{
-    color: black;
-    font-family: myriad-pro, sans-serif;
-    font-style: normal;
-    letter-spacing: 20px;
-}
+      recorder = RecordRTC(camera, {
+         type: 'video'
+      });
 
-.subtitle{
-    color: black;
-    font-family: myriad-pro, sans-serif;
-    font-style: normal;
-    letter-spacing: 10px;
-}
+      recorder.startRecording();
 
-.font-five{
-    font-size: 1vw;
-    letter-spacing: 0.4vw;;
-}
+      // release camera on stopRecording
+      recorder.camera = camera;
 
-.font-ten{
-    font-size: 1.75vw !important;
-}
+      document.getElementById('btn-stop-recording').disabled = false;
+   });
+};
 
-.font-twenty{
-    font-size: 2.5vw;
-}
-
-.font-thirty{
-    font-size: 5vw;
-}
-
-.great-vibes-font{
-    font-family: 'Great Vibes';
-    letter-spacing: 0.2vw;
-}
-
-.center {
-    text-align: center;
-}
-
-.margin{
-    margin: 5vw 10vw;
-}
-
-.margin-thirty{
-    margin: 30px 0 0 0;
-}
-
-.margin-ninety{
-    margin: 90px 0 0 0;
-}
-
-.margin-bottom-thirty{
-    margin: 0 0 30px 0;
-}
-
-.margin-bottom{
-    margin: 0 0 90px 0;
-}
-.video-container {
-    text-align: center;
-}
-#videoElement {
-    width: 60%;
-    height: auto;
-    background-color: #666;
-}
-
-.button{
-    border: none;
-    color: #666;
-    padding: 15px 32px;
-    text-align: center;
-    align-self: center;
-    align-items: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-}
-
-.download-video-button {
-    font-size: 12px;
-    font-family: myriad-pro, sans-serif;
-    font-style: normal;
-    letter-spacing: 0.6vw;
-}
-
-.button-grid{
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 5vw;
-    grid-gap: 0;
-    text-align: center;
-}
-
-.button-grid__item--1{
-    grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 1;
-    grid-row-end: 2;
-    object-fit: cover;
-}
-
-.button-grid__item--2{
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 1;
-    grid-row-end: 2;
-    object-fit: cover;
-}
-
-.padding-zero{
-    padding: 0;
-    margin: 0;
-}
-
-.padding-ninety{
-    padding: 15vw;
-}
-
-.intro-grid__item--2{
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 1;
-    grid-row-end: 2;
-    object-fit: cover;
-}
-
-.banner-grid{
-    margin-top: 0;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 100vh;
-    align-items: center;
-}
-
-.banner-grid__item--1{
-    grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 1;
-    grid-row-end: 2;
-    object-fit: cover;
-}
+document.getElementById('btn-stop-recording').onclick = function () {
+   this.disabled = true;
+   this.style.background = "#f0f0f0";
+   this.style.color = "#666";
+   document.querySelector('#btn-start-recording').style.background = "#2ecc71";
+   document.querySelector('#btn-start-recording').style.color = "#fff";
+   document.querySelector('#btn-start-recording').disabled = false;
+   recorder.stopRecording(stopRecordingCallback);
+};
